6c931e28ed09f2842bd3c77726902d1fa819c59b
nana-hoka <75403674+nana-hoka@users.noreply.github.com>
2020-12-25 17:55:04 +0900

    Update main.js

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ function randomValueFromArray(array){
 }
 
 let storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
-let insertX = ['ジャッキー','リラックマ','ダッフィー'];
+let insertX = ['Willy the Goblin','Big Daddy','Father Christmas'];
 let insertY = ['the soup kitchen','Disneyland','the White House'];
 let insertZ = ['spontaneously combusted','melted into a puddle on the sidewalk','turned into a slug and crawled away'];
 
@@ -28,10 +28,10 @@ function result() {
 
   if(customName.value !== '') {
     const name = customName.value;
-    newStory = newStory.replace('梅子',name);
+    newStory = newStory.replace('Bob',name);
   }
 
-  if(document.getElementById("日本").checked) {
+  if(document.getElementById("uk").checked) {
     const weight = Math.round(300*0.0714286) + ' stone';
     const temperature =  Math.round((94-32) * 5 / 9) + ' centigrade';
     newStory = newStory.replace('94 fahrenheit',temperature);
