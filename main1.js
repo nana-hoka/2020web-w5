3892076b0780685d14de818daf509afb96704be1
nana-hoka <75403674+nana-hoka@users.noreply.github.com>
2020-12-25 21:34:25 +0900

    Update main1.js

--- main1.js
+++ main1.js
@@ -23,7 +23,7 @@ function result() {
   newStory = newStory.replace(':insertx:',xItem);
   newStory = newStory.replace(':insertx:',xItem);
   newStory = newStory.replace(':inserty:',yItem);
-  newStory = newStory.replace(':inserty:',yItem);
+  
 
   if(customName.value !== '') {
     const name = customName.value;
