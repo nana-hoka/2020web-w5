cf63fef5df8bad823ce8122054fa9b89644acf51
nana-hoka <75403674+nana-hoka@users.noreply.github.com>
2020-12-25 21:07:56 +0900

    Update main1.js

--- main1.js
+++ main1.js
@@ -7,7 +7,7 @@ function randomValueFromArray(array){
   return array[random];
 }
 
-let storyText = '気温　摂氏-1度で外は寒かった。:insertx: は出かけた。 :inserty:についた。混雑していた。体重50ポンドの:insertx:は驚いたが、梅子は驚かなかった。:inserty:ではよくあることだった。 ';
+let storyText = '気温　摂氏-1度で外は寒かった。:insertx: は出かけた。 :inserty:についた。混雑していた。体重100ポンドの:insertx:は驚いたが、梅子は驚かなかった。:inserty:ではよくあることだった。 ';
 let insertX = ['ジャッキー','リラックマ','ダッフィー'];
 let insertY = ['琵琶湖','富士山','信濃川'];
 
@@ -34,8 +34,8 @@ function result() {
   if(document.getElementById("日本").checked) {
     const weight = Math.round(300*0.0714286) + ' kg';
     const temperature = '華氏 '+ Math.round((94-32) * 5 / 9) + ' 度';
-    newStory = newStory.replace('40',temperature);
-    newStory = newStory.replace('50',weight);
+    newStory = newStory.replace('摂氏-1度',temperature);
+    newStory = newStory.replace('100',weight);
   }
 
   story.textContent = newStory;
