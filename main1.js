ba050d6e094b7b540ecfe1039f8637507e9b1b6a
nana-hoka <75403674+nana-hoka@users.noreply.github.com>
2021-01-02 20:32:52 +0900

    Update main1.js

--- main1.js
+++ main1.js
@@ -7,9 +7,9 @@ function randomValueFromArray(array){
   return array[random];
 }
 
-let storyText = '気温 摂氏-1度で外は寒かった。この日は:insertx:のライブが:inserty:であった。:insertx:のファンの友達に誘われて来ていた梅子は、たまたま隣の席にいた年俸5000万ドルの野球選手に夢中だった。';
+let storyText = '気温 摂氏-1度で外は寒かった。この日は:insertx:のライブが:inserty:であった。ファンの友達に誘われて来ていた梅子は:insertx:にはあまり興味がなかった。梅子はスポーツ全般に興味を持っていて、この日のライブに来たのも:inserty:に行ってみたかったからだ。そんな梅子に驚く出来事が！！それは、たまたま隣の席に年俸5000万ドルの野球選手がいた。梅子はライブどころではなかった。';
 let insertX = ['嵐','back number','Perfume','ゆず','NiziU']];
-let insertY = ['東京ドーム','日本武道館','国立競技場'];
+let insertY = ['東京ドーム','国立競技場'];
 
 
 randomize.addEventListener('click', result);
@@ -23,6 +23,7 @@ function result() {
 
   newStory = newStory.replace(':insertx:',xItem);
   newStory = newStory.replace(':insertx:',xItem);
+  newStory = newStory.replace(':inserty:',yItem);
   newStory = newStory.replace(':inserty:',yItem);
   
 
