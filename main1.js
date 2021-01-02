2fcd573281e385e6caa3d0a02d7d28fe8e6c8775
nana-hoka <75403674+nana-hoka@users.noreply.github.com>
2021-01-02 20:36:33 +0900

    Update main1.js

--- main1.js
+++ main1.js
@@ -7,9 +7,10 @@ function randomValueFromArray(array){
   return array[random];
 }
 
-let storyText = '気温 摂氏-1度で外は寒かった。この日は:insertx:のライブが:inserty:であった。ファンの友達に誘われて来ていた梅子は:insertx:にはあまり興味がなかった。梅子はスポーツ全般に興味を持っていて、この日のライブに来たのも:inserty:に行ってみたかったからだ。そんな梅子に驚く出来事が！！それは、たまたま隣の席に年俸5000万ドルの野球選手がいた。梅子はライブどころではなかった。';
+let storyText = '気温 摂氏-1度で外は寒かった。この日は:insertx:のライブが:inserty:であった。ファンの友達に誘われて来ていた梅子は:insertx:にはあまり興味がなかった。梅子はスポーツ全般に興味を持っていて、この日のライブに来たのも:inserty:に行ってみたかったからだ。そんな梅子に驚く出来事が！！それは、たまたま隣の席に年俸5000万ドルのプロ:insertz:選手がいた。梅子はライブどころではなかった。';
 let insertX = ['嵐','back number','Perfume','ゆず','NiziU']];
 let insertY = ['東京ドーム','国立競技場'];
+let insertZ = ['野球','サッカー','バスケットボール','卓球','ゴルフ'];
 
 
 randomize.addEventListener('click', result);
@@ -19,13 +20,13 @@ function result() {
 
   let xItem = randomValueFromArray(insertX);
   let yItem = randomValueFromArray(insertY);
-  
+  let zItem = randomValueFromArray(insertZ);
 
   newStory = newStory.replace(':insertx:',xItem);
   newStory = newStory.replace(':insertx:',xItem);
   newStory = newStory.replace(':inserty:',yItem);
   newStory = newStory.replace(':inserty:',yItem);
-  
+  newStory = newStory.replace(':inserty:',zItem);
 
   if(customName.value !== '') {
     const name = customName.value;
