f80f5e94719ed36f313fb36b0ddfc64c6e3e9f58
nana-hoka <75403674+nana-hoka@users.noreply.github.com>
2021-01-03 13:57:32 +0900

    Update main1.js

--- main1.js
+++ main1.js
@@ -37,7 +37,7 @@ function result() {
     const price = Math.round(1) + ' 000万ドル';
     const temperature = '華氏 '+ Math.round((94-32) * 5 / 9) + ' 度';
     newStory = newStory.replace('摂氏-1度',temperature);
-    newStory = newStory.replace('5000万ドル',price);
+    newStory = newStory.replace('5億円',price);
   }
 
   story.textContent = newStory;
